bcc7cdedd0b827d599a53ad9e3c457569851e44a
Angel Ponce <60164099+Angel-Ponce@users.noreply.github.com>
2021-01-23 14:48:45 -0600

    Tile structure

--- Schedule/src/ScheduleStructure/Tile.java
+++ Schedule/src/ScheduleStructure/Tile.java
@@ -1,16 +1,93 @@
 package ScheduleStructure;
 
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JLabel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 /**
  *
  * @author angelponce
  */
-public class Tile {
-    
+public class Tile extends JLabel {
+
+    private String type;
+    private String courseName;
+    private String url;
+    private String hour;
+
+    public Tile(String type, String courseName, String url, String hour, Color background) {
+        this.type = type;
+        this.courseName = courseName;
+        this.url = url;
+        this.hour = hour;
+        this.setBackground(Color.WHITE);
+    }
+
+    private void events() {
+        this.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
 }
