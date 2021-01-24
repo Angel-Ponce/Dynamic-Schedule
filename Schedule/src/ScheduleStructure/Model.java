c5f58c647eb3f9f35ad5895e09c5a82e56a18f56
= <acdpf000@gmail.com>
2021-01-24 13:19:34 -0600

    Model for read files.txt

--- Schedule/src/ScheduleStructure/Model.java
+++ Schedule/src/ScheduleStructure/Model.java
@@ -1,16 +1,66 @@
 package ScheduleStructure;
 
+import java.awt.Color;
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 /**
  *
  * @author angelponce
  */
 public class Model {
-    
+
+    private Txt properties = new Txt("properties");
+    private Txt courses = new Txt("courses");
+
+    public void saveProperties(int rowCount, String theme) {
+        ArrayList<String> lines = new ArrayList();
+        lines.add(String.valueOf(rowCount));
+        lines.add(theme);
+        properties.setLines(lines);
+    }
+
+    public ArrayList<String> getProperties() {
+        ArrayList<String> properties = new ArrayList();
+        properties = this.properties.getLines();
+        return properties;
+    }
+
+    public void saveCourses(ArrayList<Row> rows) {
+        ArrayList<String> lines = new ArrayList();
+        for (Row row : rows) {
+            String rowString = row.hour.getHour() + "," + row.hour.getBackground().getRGB() + ";";
+            for (Tile tile : row.days) {
+                rowString += tile.getCourseName() + "," + tile.getUrl() + "," + tile.getBackground().getRGB() + ";";
+            }
+            lines.add(rowString);
+        }
+        courses.setLines(lines);
+    }
+
+    public ArrayList<Row> getRows() {
+        ArrayList<Row> rows = new ArrayList();
+        ArrayList<String> rowsString = this.courses.getLines();
+        String[] tilesString = null;
+        for (String row : rowsString) {
+            ArrayList<Tile> tiles = new ArrayList();
+            tilesString = row.split(";");
+            String[] tileHour = tilesString[0].split(",");
+            Tile hour = new Tile(Tile.HOUR, "", "", tileHour[0], new Color(Integer.parseInt(tileHour[1])));
+            tiles.add(hour);
+            for (int i = 1; i < tilesString.length; i++) {
+                String[] dayTile = tilesString[i].split(",");
+                Tile day = new Tile(Tile.COURSE, dayTile[0], dayTile[1], "", new Color(Integer.parseInt(dayTile[2])));
+                tiles.add(day);
+            }
+            
+            Row newRow = new Row(tiles.get(0), tiles.get(1), tiles.get(2), tiles.get(3), tiles.get(4), tiles.get(5));
+            rows.add(newRow);
+        }
+        return rows;
+    }
 }
--- Schedule/src/ScheduleStructure/Row.java
+++ Schedule/src/ScheduleStructure/Row.java
@@ -23,12 +23,26 @@ public class Row {
     ArrayList<Tile> days = new ArrayList();
 
     public Row(Theme th) {
-        hour = new Tile(Tile.HOUR, null, null, "Hora", th.hoursColor);
-        monday = new Tile(Tile.COURSE, "Curso", "url", null, th.coursesColor);
-        tuesday = new Tile(Tile.COURSE, "Curso", "url", null, th.coursesColor);
-        wednesday = new Tile(Tile.COURSE, "Curso", "url", null, th.coursesColor);
-        thursday = new Tile(Tile.COURSE, "Curso", "url", null, th.coursesColor);
-        friday = new Tile(Tile.COURSE, "Curso", "url", null, th.coursesColor);
+        hour = new Tile(Tile.HOUR, "", "", "Hora", th.hoursColor);
+        monday = new Tile(Tile.COURSE, "Curso", "url", "", th.coursesColor);
+        tuesday = new Tile(Tile.COURSE, "Curso", "url", "", th.coursesColor);
+        wednesday = new Tile(Tile.COURSE, "Curso", "url", "", th.coursesColor);
+        thursday = new Tile(Tile.COURSE, "Curso", "url", "", th.coursesColor);
+        friday = new Tile(Tile.COURSE, "Curso", "url", "", th.coursesColor);
+        days.add(monday);
+        days.add(tuesday);
+        days.add(wednesday);
+        days.add(thursday);
+        days.add(friday);
+    }
+
+    public Row(Tile hour, Tile monday, Tile tuesday, Tile wednesday, Tile thursday, Tile friday) {
+        this.hour = hour;
+        this.monday = monday;
+        this.tuesday = tuesday;
+        this.wednesday = wednesday;
+        this.thursday = thursday;
+        this.friday = friday;
         days.add(monday);
         days.add(tuesday);
         days.add(wednesday);
